253db5e3bd1fd8f424604fc2571b01c411b8a186
Simonbk22 <jjjbbk0@gmail.com>
2022-11-14 20:39:10 +0900

    상세페이지, 주문하기 테스트 푸시

--- src/main/java/kr/co/lookst/shop/ShopController.java
+++ src/main/java/kr/co/lookst/shop/ShopController.java
@@ -1,8 +1,21 @@
 package kr.co.lookst.shop;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/shop")
 public class ShopController {
 
+	@GetMapping("/order")
+	public String snsForm() {
+		return "shop/order";
+	}
+	@GetMapping("/productdetail")
+	public String myProfile() {
+		return "shop/productdetail";
+	}
+	
+	
 }
