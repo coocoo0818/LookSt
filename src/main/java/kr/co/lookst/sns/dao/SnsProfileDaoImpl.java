e84992c93b026189707d8e21af55fe54b1b1b412
jjhyeonjj <jjh4523a@gmail.com>
2022-11-15 17:25:06 +0900

    sns profile화면(db select) / 이미지 추가

---
+++ src/main/java/kr/co/lookst/sns/dao/SnsProfileDaoImpl.java
@@ -0,0 +1,31 @@
+package kr.co.lookst.sns.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.lookst.sns.domain.ProfileFeedDto;
+import kr.co.lookst.sns.domain.SnsProfileDto;
+
+@Repository
+public class SnsProfileDaoImpl implements SnsProfileDao {
+
+	@Autowired
+	private SqlSession session;
+	private static String namespace = "kr.co.lookst.sns.dao.snsMapper.";
+	
+	@Override
+	public SnsProfileDto infoselect(String member_id) throws Exception {
+		return session.selectOne(namespace+"profileInfo", member_id);
+	}
+
+	@Override
+	public List<ProfileFeedDto> profileFeed(String member_id) throws Exception {
+		return session.selectList(namespace+"profileFeed", member_id);
+	}
+	
+
+
+}
--- src/main/java/kr/co/lookst/sns/domain/SnsDto.java
+++
@@ -1,5 +0,0 @@
-package kr.co.lookst.sns.domain;
-
-public class SnsDto {
-
-}
