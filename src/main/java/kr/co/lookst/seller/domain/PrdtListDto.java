e9dfadba2df0b8b1a2775f2b2247f69486b9f77c
bong2ee <qhduddlfkda@gmail.com>
2023-01-12 09:51:56 +0900

    상품 업로드 완료

--- src/main/java/kr/co/lookst/seller/domain/PrdtListDto.java
+++ src/main/java/kr/co/lookst/seller/domain/PrdtListDto.java
@@ -1,7 +1,15 @@
 package kr.co.lookst.seller.domain;
 
-import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PrdtListDto {
 
 	private int product_no;
@@ -13,143 +21,7 @@ public class PrdtListDto {
 	private int product_price;
 	private String prdt_option_stock;
 	private String product_type;
-	
-	
-	public PrdtListDto() {
-		// TODO Auto-generated constructor stub
-	}
-
-
-	public PrdtListDto(int product_no, String prdt_img_name, String product_kind, String product_name,
-			String prdt_option_size, String prdt_option_color, int product_price, String prdt_option_stock,
-			String product_type) {
-		super();
-		this.product_no = product_no;
-		this.prdt_img_name = prdt_img_name;
-		this.product_kind = product_kind;
-		this.product_name = product_name;
-		this.prdt_option_size = prdt_option_size;
-		this.prdt_option_color = prdt_option_color;
-		this.product_price = product_price;
-		this.prdt_option_stock = prdt_option_stock;
-		this.product_type = product_type;
-	}
-
-
-	public int getProduct_no() {
-		return product_no;
-	}
-
-
-	public void setProduct_no(int product_no) {
-		this.product_no = product_no;
-	}
-
-
-	public String getPrdt_img_name() {
-		return prdt_img_name;
-	}
-
-
-	public void setPrdt_img_name(String prdt_img_name) {
-		this.prdt_img_name = prdt_img_name;
-	}
-
-
-	public String getProduct_kind() {
-		return product_kind;
-	}
-
-
-	public void setProduct_kind(String product_kind) {
-		this.product_kind = product_kind;
-	}
-
-
-	public String getProduct_name() {
-		return product_name;
-	}
-
-
-	public void setProduct_name(String product_name) {
-		this.product_name = product_name;
-	}
-
-
-	public String getPrdt_option_size() {
-		return prdt_option_size;
-	}
-
-
-	public void setPrdt_option_size(String prdt_option_size) {
-		this.prdt_option_size = prdt_option_size;
-	}
-
-
-	public String getPrdt_option_color() {
-		return prdt_option_color;
-	}
-
-
-	public void setPrdt_option_color(String prdt_option_color) {
-		this.prdt_option_color = prdt_option_color;
-	}
-
-
-	public int getProduct_price() {
-		return product_price;
-	}
-
-
-	public void setProduct_price(int product_price) {
-		this.product_price = product_price;
-	}
-
-
-	public String getPrdt_option_stock() {
-		return prdt_option_stock;
-	}
-
-
-	public void setPrdt_option_stock(String prdt_option_stock) {
-		this.prdt_option_stock = prdt_option_stock;
-	}
-
-
-	public String getProduct_type() {
-		return product_type;
-	}
-
-
-	public void setProduct_type(String product_type) {
-		this.product_type = product_type;
-	}
-
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(prdt_img_name, prdt_option_color, prdt_option_size, prdt_option_stock, product_kind,
-				product_name, product_no, product_price, product_type);
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PrdtListDto other = (PrdtListDto) obj;
-		return Objects.equals(prdt_img_name, other.prdt_img_name)
-				&& Objects.equals(prdt_option_color, other.prdt_option_color)
-				&& Objects.equals(prdt_option_size, other.prdt_option_size)
-				&& Objects.equals(prdt_option_stock, other.prdt_option_stock)
-				&& Objects.equals(product_kind, other.product_kind) && Objects.equals(product_name, other.product_name)
-				&& product_no == other.product_no && product_price == other.product_price
-				&& Objects.equals(product_type, other.product_type);
-	}
-	
+	private String seller_no;
+	private String member_id;
 	
 }
